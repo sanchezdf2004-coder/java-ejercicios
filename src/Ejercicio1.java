5cc04361d42cc7457c871a0b16aa07ccf0d9c240
David <sanchezdf2004@gmail.com>
2026-06-23 16:03:08 -0500

    primer commit - ejercicios 1 y 2

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+class Ejercicio1 {
+    public static void main(String[] args){
+
+        String nombre = "Levia";
+        int edad = 22;
+        String carrera = "Ingenieria en sistemas";
+
+        System.out.println("hOLA MUNDO, SOY " + nombre + " mi edad es " + edad
+        + " y estudio " + carrera
+        );
+
+    }
+
+
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,18 @@
+class Ejercicio2 {
+    public static void main (String []args) {
+        int edad = 70;
+
+        if (edad >= 18 && edad <=20) {
+
+            System.out.println("Es mayor de edad");}
+
+        else if (edad>65) {
+
+            System.out.println("eres adulto mayor");
+
+        } else{
+                System.out.println("Es menor de edad");
+
+            }
+    }
+}
