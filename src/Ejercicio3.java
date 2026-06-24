822e1758bc6d804d44b0c4cb50a37a102f100c0f
David <sanchezdf2004@gmail.com>
2026-06-23 19:52:46 -0500

    ejercicio3 -bucles for y while

---
+++ src/Ejercicio3.java
@@ -0,0 +1,23 @@
+class Ejercicio3 {
+
+    public static void main (String [] args){
+
+        for (int i=1;i<11;i++){
+            System.out.println(i);
+        }
+
+//bucle while
+        System.out.println();
+        System.out.println("bucle while ahsta de numeros pares");
+int j=1;
+        while (j <=20) {
+            if (j %2 ==0) {
+                System.out.println(j);
+            }
+       j++;
+            }
+
+
+
+    }
+}
